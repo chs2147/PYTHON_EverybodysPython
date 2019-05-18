c52dc9e3c70f5342ef2d12de4b200ec2f9c8b164
parallels <chs2147@gmail.com>
2019-05-18 21:33:29 +0900

    Initial commit.

---
+++ print_colored/print_colored.py
@@ -0,0 +1,13 @@
+# Install 'termcolor' first
+# https://pypi.org/project/termcolor/
+
+from termcolor import colored
+
+print(colored('Hello World !!', 'red'))
+print(colored('Hello World !!', 'cyan'))
+print(colored('Hello World !!', 'blue'))
+print(colored('Hello World !!', 'yellow'))
+print(colored('Hello World !!', 'green'))
+print(colored('Hello World !!', 'grey'))
+print(colored('Hello World !!', 'magenta'))
+print(colored('Hello World !!', 'white'))
