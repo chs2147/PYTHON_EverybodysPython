123460fa7f1d3a5951a2513261ea11843a59a2a0
parallels <chs2147@gmail.com>
2019-05-27 16:54:38 +0900

    Initial commit.

--- api_gateway/flask/flask-restful_api_gateway.py
+++ api_gateway/flask/flask-restful_api_gateway.py
@@ -72,3 +72,4 @@ api.add_resource(Todo, '/todos/<todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- api_gateway/flask/flask_api_gateway_humans.py
+++ api_gateway/flask/flask_api_gateway_humans.py
@@ -66,3 +66,4 @@ def delete_one(name):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
